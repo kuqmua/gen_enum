021da19133d24e781df038ec0beea9e215741bfc
kuqmua <serezhashmakov@outlook.com>
2022-11-24 21:20:47 +0300

    clippy

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,11 @@
+#![deny(
+    clippy::indexing_slicing,
+    clippy::integer_arithmetic,
+    clippy::unwrap_used,
+    clippy::float_arithmetic
+)]
+#![allow(clippy::too_many_arguments)]
+
 #[proc_macro_derive(GenEnum)]
 pub fn derive_gen_enum(input: proc_macro::TokenStream) -> proc_macro::TokenStream {
     use convert_case::Case;
